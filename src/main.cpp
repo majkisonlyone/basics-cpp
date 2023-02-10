e3c7df7bc3921a0619eb05e72ccf8461f8c252c2
Maja UwU <nabielskamaja@gmail.com>
2023-02-10 01:02:52 +0100

    printing rectangles

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 
 using namespace std;
+//input *23 output prostokąt gwiazdek szer. 2 wys. 3
 
+void wprowadzanie( int& a, char& zn, int& h)
+{
+    cout<<"wprowadz szerokosc, znak i wysokosc prostokata...\n";
+    cin>>a>>zn>>h;
+}
+void figura()
+{
+    cout<<"twoj prostokat: \n";
+}
+void konstrukcja(int szerokosc, char znak, int wysokosc)
+{
+    for(int powtorzenia = 0; powtorzenia<wysokosc; powtorzenia++)
+    {
+        for(int powtorzenia = 0; powtorzenia<szerokosc; powtorzenia++)
+        {
+        cout<<znak;
+        }
+        cout<<"\n";
+    }
+    cout<<"\n";
+}
 int main ()
 {
-    cout<< "hello world";
+    char znak;
+    int szerokosc, wysokosc;
+    wprowadzanie(szerokosc, znak, wysokosc);
+    figura();
+    konstrukcja(szerokosc, znak, wysokosc);
+
     return 0;
 }
